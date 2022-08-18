3c3516c80a2eb8c6bc42912016ce6cb88892cd1a
palminski <williambolls98@gmail.com>
2022-08-17 22:21:38 -0700

    basic information added

---
+++ assets/js/index.js
@@ -0,0 +1,3 @@
+//HTML elements on page. These may be unneeded if we use jQuerry
+const $locationInput = document.querySelector("#location-input");
+const $breweryInput = document.querySelector("#brewery-input");
