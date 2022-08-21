5dc209a415dccd0545b09a0d426b9896349c0783
palminski <williambolls98@gmail.com>
2022-08-21 00:20:46 -0700

    added the CDNs for following: jQuerry, jQuerryUI, and semantic UI Additionally, I included a fetch request to the Open Brewery API to ensure that we can recieve json data from it. to test, enter a search term in the function in .js file and view console log on page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
 //HTML elements on page. These may be unneeded if we use jQuerry
 const $locationInput = document.querySelector("#location-input");
 const $breweryInput = document.querySelector("#brewery-input");
+
+//Sample UI Request to use for testing
+//Will take sample term and console log JSON data
+const searchBrewery = (searchTerm) => {
+    const API_URL = "https://api.openbrewerydb.org/breweries/search?query="
+    fetch(API_URL + searchTerm).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+    });
+}
+searchBrewery("Eugene");
