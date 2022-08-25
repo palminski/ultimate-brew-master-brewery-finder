00f3451a00e0c974e700ccafd4eaa9af21d3c11b
deand925 <97762768+deand925@users.noreply.github.com>
2022-08-25 12:28:40 -0700

    deleted long quotes, added mediaqueries

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,14 +4,6 @@ const $breweryInput = document.querySelector("#brewery-input");
 
 
 const beerQuotes = [
-    {
-       quote: '"Humankind was built on beer. From the world’s first writing to its first laws, in rituals social, religious, and political, civilization is soaked in beer.”',
-       author: '–William Bostwick'
-    },
-    {
-        quote: '“So when the devil says to you, ‘Do not drink,’ answer him: ‘I will drink, and right freely, just because you tell me not to.’ One must always do what Satan forbids.”',
-        author: '— Martin Luther'
-     },
      {
         quote: '“The mouth of a perfectly happy man is filled with beer.”',
         author: '-Ancient Egyptian Wisdom, 2200 B.C.'
@@ -28,22 +20,6 @@ const beerQuotes = [
          quote: '“Many battles have been fought and won by soldiers nourished on beer.”',
          author: '—Frederick the Great'
       },
-      {
-         quote: '“This is grain, which any fool can eat, but for which the lord has intended a more divine form of consumption. Let us give praise to our maker and glory to His bounty by learning about beer.”',
-         author: '–Friar Tuck, Robin Hood'
-      },
-      {
-          quote: '“Thirstily he set it to his lips, and as its cool refreshment began to soothe his throat, he thanked Heaven that in a world of much evil there was still so good a thing as ale.”',
-          author: '–Rafael Sabatini,  Fortune’s Fool'
-       },
-       {
-        quote: '“There is nothing which has yet been contrived by man by which so much happiness is produced as by a good tavern or inn.”',
-        author: '-Samuel Johnson'
-     },
-     {
-        quote: '“We could not now take time for further search or consideration: our victuals being much spent, especially our beer.”',
-        author: '–Mayflower logbook entry dated December 16, 1620'
-     },
      {
          quote: '“Beer, if drank with moderation, softens the temper, cheers the spirit and promotes health.”',
          author: '-Thomas Jefferson'
@@ -79,10 +55,6 @@ const beerQuotes = [
      {
         quote: '“For a quart of Ale is a dish for a king.”',
         author: '-William Shakespeare'
-     },
-     {
-        quote: '“Without question, the greatest invention in the history of mankind is beer. Oh, I grant you that the wheel was also a fine invention, but the wheel does not go nearly as well with pizza.”',
-        author: '-Dave Berry'
      }
 ]
 
