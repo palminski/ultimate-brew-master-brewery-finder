34f03690271b8d725b1f9830e1da510d92321ee5
deand925 <97762768+deand925@users.noreply.github.com>
2022-08-25 09:00:37 -0700

    created array of objects for random quotes

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,88 @@
 const $locationInput = document.querySelector("#location-input");
 const $breweryInput = document.querySelector("#brewery-input");
 
-
+const beerQuotes = [
+    {
+       "quote": '"Humankind was built on beer. From the world’s first writing to its first laws, in rituals social, religious, and political, civilization is soaked in beer.”',
+       "author": '–William Bostwick'
+    },
+    {
+        "quote": '“So when the devil says to you, ‘Do not drink,’ answer him: ‘I will drink, and right freely, just because you tell me not to.’ One must always do what Satan forbids.”',
+        "author": '— Martin Luther'
+     },
+     {
+        "quote": '“The mouth of a perfectly happy man is filled with beer.”',
+        "author": '-Ancient Egyptian Wisdom, 2200 B.C.'
+     },
+     {
+         "quote": '“He was a wise man who invented beer”',
+         "author": '–Plato'
+      },
+      {
+        "quote": '“Fermentation may have been a greater discovery than fire.”',
+        "author": '–David Rains Wallace'
+     },
+     {
+         "quote": '“Many battles have been fought and won by soldiers nourished on beer.”',
+         "author": '—Frederick the Great'
+      },
+      {
+         "quote": '“This is grain, which any fool can eat, but for which the lord has intended a more divine form of consumption. Let us give praise to our maker and glory to His bounty by learning about beer.”',
+         "author": '–Friar Tuck, Robin Hood'
+      },
+      {
+          "quote": '“Thirstily he set it to his lips, and as its cool refreshment began to soothe his throat, he thanked Heaven that in a world of much evil there was still so good a thing as ale.”',
+          "author": '–Rafael Sabatini,  Fortune’s Fool'
+       },
+       {
+        "quote": '“There is nothing which has yet been contrived by man by which so much happiness is produced as by a good tavern or inn.”',
+        "author": '-Samuel Johnson'
+     },
+     {
+        "quote": '“We could not now take time for further search or consideration: our victuals being much spent, especially our beer.”',
+        "author": '–Mayflower logbook entry dated December 16, 1620'
+     },
+     {
+         "quote": '“Beer, if drank with moderation, softens the temper, cheers the spirit and promotes health.”',
+         "author": '-Thomas Jefferson'
+      },
+      {
+        "quote": '“We should look for someone to eat and drink with before looking for something to eat and drink.”',
+        "author": '—Epicurus'
+     },
+     {
+        "quote": '“Let no man thirst for good beer.”',
+        "author": '–Sam Adams'
+     },
+     {
+         "quote": '“Give me a woman who loves beer and I will conquer the world.”',
+         "author": '–Kaiser Wilhelm'
+      },
+      {
+        "quote": '“Prohibition makes you want to cry into your beer and denies you the beer to cry into.”',
+        "author": '—Don Marquis'
+     },
+     {
+       "quote": '“On victory, you deserve beer, in defeat, you need it.”',
+       "author": '—Napoleon'
+    },
+    {
+       "quote": '“Beer, it’s the best damn drink in the world.”',
+       "author": '-Jack Nicholson'
+    },
+    {
+        "quote": '“There is no such thing as a bad beer. It’s that some taste better than others.”',
+        "author": '-Bill Carter'
+     },
+     {
+        "quote": '“For a quart of Ale is a dish for a king.”',
+        "author": '-William Shakespeare'
+     },
+     {
+        "quote": '“Without question, the greatest invention in the history of mankind is beer. Oh, I grant you that the wheel was also a fine invention, but the wheel does not go nearly as well with pizza.”',
+        "author": '-Dave Berry'
+     }
+]
     
 
 
