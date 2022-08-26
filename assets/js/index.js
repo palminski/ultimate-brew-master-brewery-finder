b7e9d44721ca3f4328ed16ade5456d9737382ac7
Travis Geers <armyman1992@hotmail.com>
2022-08-26 15:10:04 -0600

    added map fetch data and fixed some typos

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,65 +5,65 @@ const $breweryInput = document.querySelector("#brewery-input");
 
 const beerQuotes = [
     {
-       quote: '“The mouth of a perfectly happy man is filled with beer.”',
-       author: '-Ancient Egyptian Wisdom, 2200 B.C.'
+        quote: '“The mouth of a perfectly happy man is filled with beer.”',
+        author: '-Ancient Egyptian Wisdom, 2200 B.C.'
     },
     {
         quote: '“He was a wise man who invented beer”',
         author: '–Plato'
-     },
-     {
-       quote: '“Fermentation may have been a greater discovery than fire.”',
-       author: '–David Rains Wallace'
+    },
+    {
+        quote: '“Fermentation may have been a greater discovery than fire.”',
+        author: '–David Rains Wallace'
     },
     {
         quote: '“Many battles have been fought and won by soldiers nourished on beer.”',
         author: '—Frederick the Great'
-     },
+    },
     {
         quote: '“Beer, if drank with moderation, softens the temper, cheers the spirit and promotes health.”',
         author: '-Thomas Jefferson'
-     },
-     {
-       quote: '“We should look for someone to eat and drink with before looking for something to eat and drink.”',
-       author: '—Epicurus'
     },
     {
-       quote: '“Let no man thirst for good beer.”',
-       author: '–Sam Adams'
+        quote: '“We should look for someone to eat and drink with before looking for something to eat and drink.”',
+        author: '—Epicurus'
+    },
+    {
+        quote: '“Let no man thirst for good beer.”',
+        author: '–Sam Adams'
     },
     {
         quote: '“Give me a woman who loves beer and I will conquer the world.”',
         author: '–Kaiser Wilhelm'
-     },
-     {
-       quote: '“Prohibition makes you want to cry into your beer and denies you the beer to cry into.”',
-       author: '—Don Marquis'
     },
     {
-      quote: '“On victory, you deserve beer, in defeat, you need it.”',
-      author: '—Napoleon'
-   },
-   {
-      quote: '“Beer, it’s the best damn drink in the world.”',
-      author: '-Jack Nicholson'
-   },
-   {
-       quote: '“There is no such thing as a bad beer. It’s that some taste better than others.”',
-       author: '-Bill Carter'
+        quote: '“Prohibition makes you want to cry into your beer and denies you the beer to cry into.”',
+        author: '—Don Marquis'
     },
     {
-       quote: '“For a quart of Ale is a dish for a king.”',
-       author: '-William Shakespeare'
+        quote: '“On victory, you deserve beer, in defeat, you need it.”',
+        author: '—Napoleon'
+    },
+    {
+        quote: '“Beer, it’s the best damn drink in the world.”',
+        author: '-Jack Nicholson'
+    },
+    {
+        quote: '“There is no such thing as a bad beer. It’s that some taste better than others.”',
+        author: '-Bill Carter'
+    },
+    {
+        quote: '“For a quart of Ale is a dish for a king.”',
+        author: '-William Shakespeare'
     }
 ]
 
 const updateBeerQuote = () => {
-    quoteToDisplay = beerQuotes[randomNumber(0,beerQuotes.length-1)];
-    $("#beer-quote").html(quoteToDisplay.quote+"<br><span>"+quoteToDisplay.author+"</span>");
+    quoteToDisplay = beerQuotes[randomNumber(0, beerQuotes.length - 1)];
+    $("#beer-quote").html(quoteToDisplay.quote + "<br><span>" + quoteToDisplay.author + "</span>");
 }
 
-const searchBrewery = (locationInput,stateInput,breweryInput) => {
+const searchBrewery = (locationInput, stateInput, breweryInput) => {
     // Declaring the basis of the url to be used with the API
     // const API_URL = "https://api.openbrewerydb.org/breweries?per_page=10&by_city=san_diego&by_name=cooper";
     let locationParameter = "";
@@ -72,20 +72,20 @@ const searchBrewery = (locationInput,stateInput,breweryInput) => {
     const API_URL_BASE = "https://api.openbrewerydb.org/breweries?per_page=20";
     //Check to see if search terms were entered and then create a querry string based off the input
     if (locationInput) {
-        locationParameter = "&by_city="+locationInput;
+        locationParameter = "&by_city=" + locationInput;
     }
     if (stateInput) {
-        stateParameter = "&by_state="+stateInput;
+        stateParameter = "&by_state=" + stateInput;
     }
     if (breweryInput) {
-        breweryParameter = "&by_name="+breweryInput ;
+        breweryParameter = "&by_name=" + breweryInput;
     }
-    let API_URL = API_URL_BASE+locationParameter+stateParameter+breweryParameter;
+    let API_URL = API_URL_BASE + locationParameter + stateParameter + breweryParameter;
 
     fetch(API_URL).then(function (response) {
         return response.json();
     }).then(function (data) {
-        
+
         listBreweries(data);
 
     });
@@ -97,43 +97,52 @@ const listBreweries = (breweries) => {
 
     //Randomly shuffles an array to be used when determining the gif to use for each card
     let numberTOGrab = 50;
-        let startingArray = [];
-        for (let i =0; i < numberTOGrab; i++){
-            startingArray.push(i)
-        }
+    let startingArray = [];
+    for (let i = 0; i < numberTOGrab; i++) {
+        startingArray.push(i)
+    }
     let randomArray = shuffle(startingArray);
-   
 
-    for (let i = 0; i< breweries.length; i++) {
+
+    for (let i = 0; i < breweries.length; i++) {
         //Grab Gif
-        fetch("https://api.giphy.com/v1/gifs/search?api_key=t8B9bOhzlzT6JWigjBj02k9eDnQx1nFI&q=simpsons-beer&rating=pg&limit="+numberTOGrab).then(function (response) {
+        fetch("https://api.giphy.com/v1/gifs/search?api_key=t8B9bOhzlzT6JWigjBj02k9eDnQx1nFI&q=simpsons-beer&rating=pg&limit=" + numberTOGrab).then(function (response) {
             return response.json();
         }).then(function (data) {
-            
+           
             let gif = data.data[randomArray[i]].images.original.url;
 
+            //Grab Map Data
+            fetch("https://api.geoapify.com/v1/geocode/search?text=" + breweries[i].street + "20%" + breweries[i].city + "20%" + breweries[i].state + "&format=json&apiKey=73e458ae5dad4e35ae00feb2132fe108").then(function (response) {
+                return response.json();
+            }).then(function (data) {
+
+
         
-        //Make and append Brewery Cards
-        let $breweryCard = $("<a>")
-        .addClass("ui card brew-card")
-        .attr("href",breweries[i].website_url).attr("target","_blank")
-        .append(
-            $("<div>").addClass("ui medium bordered image")
-                .append(
-                    $("<img>").addClass("ui small image").attr("src",gif)
-                ),
-            $("<div>").addClass("content")
-                .append(
-                    $("<div>").addClass("header").text(breweries[i].name),
-                    $("<div>").addClass("type").text("Brewery Type: "+breweries[i].brewery_type),
-                    $("<div>").addClass("adress").text(breweries[i].street + " " + breweries[i].city + " " + breweries[i].state),
-                    $("<div>").addClass("number").text("Phone Number: "+ breweries[i].phone),
-                    // $("<div>").addClass("description").text("Description"), //Description not contained in JSON data
-                    // $("<div>").addClass("review").text("REVIEW"), //Review Data not contained in JSON data
-                )
-        );
-        $("#card-container").append($breweryCard); //<= places info on page
-    });
+                //Make and append Brewery Cards
+                let $breweryCard = $("<a>")
+                    .addClass("ui card brew-card")
+                    .attr("href", mapData).attr("target", "_blank")
+                    .append(
+                        $("<div>").addClass("ui medium bordered image")
+                            .append(
+                                $("<img>").addClass("ui small image").attr("src", gif)
+                            ),
+                        $("<div>").addClass("content")
+                            .append(
+                                $("<div>").addClass("header").text(breweries[i].name),
+                                $("<div>").addClass("type").text("Brewery Type: " + breweries[i].brewery_type),
+                                $("<div>").addClass("address").text(breweries[i].street + " " + breweries[i].city + " " + breweries[i].state),
+                                $("<div>").addClass("number").text("Phone Number: " + breweries[i].phone),
+                                // $("<div>").addClass("description").text("Description"), //Description not contained in JSON data
+                                // $("<div>").addClass("review").text("REVIEW"), //Review Data not contained in JSON data
+                                $("<div>").addClass("url").text(breweries[i].website_url)
+                            )
+                    );
+                $("#card-container").append($breweryCard); //<= places info on page
+            })
+        });
+
     }
 }
 
@@ -142,14 +151,14 @@ const formSubmitHandler = (event) => {
     locationInput = $("#location-input").val();
     stateInput = $("#state-input").val();
     breweryInput = $("#brewery-input").val();
-    
+
     updateBeerQuote();
-    searchBrewery(locationInput,stateInput,breweryInput);
+    searchBrewery(locationInput, stateInput, breweryInput);
 }
 
 
 let randomNumber = function (min, max) {
-    let value = Math.floor(Math.random()*(max - min +1)+min);
+    let value = Math.floor(Math.random() * (max - min + 1) + min);
     return value;
 }
 let shuffle = (array) => {
@@ -158,7 +167,7 @@ let shuffle = (array) => {
     let temp;
 
     while (i--) {
-        j = Math.floor(Math.random() * (i+1)); //grabs number between 0 and current i
+        j = Math.floor(Math.random() * (i + 1)); //grabs number between 0 and current i
 
         temp = array[i]; //holds onto item at current array spot
         array[i] = array[j];   //makes current array spot equal randomly selected slot
